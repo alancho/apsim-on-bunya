b651d4e4e67763fd0e748987954fdcb38d00723d
Alan Severini <severini.alan@gmail.com>
2024-07-10 18:50:13 +1000

    Upgrade from command line

--- 01.R
+++ 01.R
@@ -4,6 +4,7 @@ require(tidyverse)
 require(apsimx)
 require(FME)
 require(rapsimng)
+require(janitor)
 
 ## apsimx_options("../apsim.sif")
 
@@ -14,12 +15,13 @@ require(rapsimng)
 
 ## simulaciones <- apsimx("simulation.apsimx", cleanup = TRUE, xargs = argumentos)
 
+system("singularity exec /home/uqasever/apsim.sif Models --upgrade simulation.apsimx")
 system("singularity exec /home/uqasever/apsim.sif Models simulation.apsimx --single-threaded --cpu-count=1")
 
 ## system("singularity exec /home/uqasever/apsim.sif Models simulation.apsimx")
 
 simulaciones <- apsimx::read_apsimx("simulation.db")
 
-simulaciones %>% saveRDS("simulaciones.rds")
+simulaciones %>% clean_names() %>% saveRDS("simulaciones.rds")
 
 simulaciones %>% head() %>% print()
