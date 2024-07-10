7634ccf6de3c28dd3e5fb4a64a47054346719d03
Alan Severini <severini.alan@gmail.com>
2024-07-10 18:31:11 +1000

    Retrieve simulations.db

--- 01.R
+++ 01.R
@@ -18,8 +18,8 @@ require(rapsimng)
 
 system("singularity exec /home/uqasever/apsim.sif Models simulation.apsimx")
 
-## simulaciones <- read_apsimx("simulation.db")
+simulaciones <- apsimx::read_apsimx("simulation.db")
 
-## simulaciones %>% saveRDS("simulaciones.rds")
+simulaciones %>% saveRDS("simulaciones.rds")
 
-## simulaciones %>% head() %>% print()
+simulaciones %>% head() %>% print()
