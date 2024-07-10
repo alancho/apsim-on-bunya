92ed378e70c7212f5345e4a783f8d800dc2fe99a
Alan Severini <severini.alan@gmail.com>
2024-07-10 17:18:34 +1000

    Changes

--- 01.R
+++ 01.R
@@ -14,7 +14,8 @@ require(rapsimng)
 
 ## simulaciones <- apsimx("simulation.apsimx", cleanup = TRUE, xargs = argumentos)
 
-system("singularity exec apsim.sif simulation.apsimx --single-threaded --cpu-count=1")
+## system("singularity exec /home/uqasever/apsim.sif simulation.apsimx --single-threaded --cpu-count=1")
+system("singularity exec /home/uqasever/apsim.sif simulation.apsimx")
 
 simulaciones <- read_apsimx("simulation.db")
 
